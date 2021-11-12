efd0f1970270b0ed9ebfe7e020f71119683c16ea
surajadkhari <surajadkhari@gmail.com>
2021-11-12 08:37:07 +0545

    create streamcontroller, two property input -sink and output-stream

--- lib/counter_blolc.dart
+++ lib/counter_blolc.dart
@@ -1,3 +1,12 @@
-class CounterBloc{
+import 'dart:async';
+
+class CounterBloc {
+  //cretae streamcontroller
+  final stateStreamController = StreamController<int>();
+
+//input property
+  StreamSink<int> get counterSink => stateStreamController.sink;
   
+//out property
+  Stream<int> get counterStream => stateStreamController.stream;
 }
